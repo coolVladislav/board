1df9f06d52f46d99b254abc72d5e51c401b3e7ac
coolVladislav <vladislav.makarevich.97@gmail.com>
2019-10-22 20:25:55 +0300

    Create auth system

--- bmain/models.py
+++ bmain/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class MyUser(AbstractUser):
+    is_activated = models.BooleanField(default=True, db_index = True, verbose_name='Пpoweл активацию?')
+    send_шessages = models.BooleanField(default=True, verbose nаmе='Слать оповещения о новых комментариях?')
+
+    class Meta:
+        pass
